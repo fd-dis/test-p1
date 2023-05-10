fe87afd7438d8565fa3cea414d8f8a85a505f23c
fd-dis <133009451+fd-dis@users.noreply.github.com>
2023-05-10 10:44:42 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// const arr = [1, 2,4,5, 'a','B','C']
+// const container = document.querySelector('.squar')
+
+// let html ='';
+
+// arr.map((test, index)=>{
+
+
+  
+// console.log(html += `<p>${test}<p>
+// <li>${index}</li>
+
+// ` );
+// // console.log(test);
+// })
+
+// container.innerHTML = html
+
+
+
+// setInterval(() => {
+  
+// }, interval);
